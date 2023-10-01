e17de5e9a1d09ad0ec106677b244681a03f3e177
Jay Choy <choyzhengjay@gmail.com>
2023-10-01 18:18:46 +0100

    Added draw

--- Main/web/js/sketchpad.js
+++ Main/web/js/sketchpad.js
@@ -9,10 +9,18 @@ class SketchPad{
       `;
     container.appendChild(this.canvas);
 
+    const lineBreak =document.createElement("br");
+    container.appendChild(lineBreak);
+
+    this.undoBtn=document.createElement("button");
+    this.undoBtn.innerHTML="UNDO";
+    container.appendChild(this.undoBtn);
+
     this.ctx = this.canvas.getContext("2d");
 
-    this.path = [];
+    this.paths = [];
     this.isDrawing = false;
+    this.#redraw();
 
     this.#addEventListerners();
   }
@@ -20,30 +28,51 @@ class SketchPad{
   #addEventListerners(){
       this.canvas.onmousedown=(evt)=>{
         const mouse = this.#getMouse(evt);
-        this.path =[mouse];
+        this.paths.push([mouse]);
         this.isDrawing=true;
       }
       this.canvas.onmousemove=(evt)=>{
         if(this.isDrawing){
           const mouse = this.#getMouse(evt);
-          this.path.push(mouse);
+          const lastPath =this.paths[this.paths.length - 1]
+          lastPath.push(mouse);
           this.#redraw();
       }
     }
     this.canvas.onmouseup=()=>{
       this.isDrawing=false;
     }
+    this.canvas.ontouchstart=(evt)=>{
+      const loc=evt.touches[0];
+      this.convas.onmousedown(loc);
+    }
+    this.canvas.ontouchmove=(evt)=>{
+      const loc = evt.touches[0];
+      this.convas.onmousedown(loc);
+    }
+    this.canvas.ontouchend=()=>{
+      this.canvas.onmouseup();
+    }
+    this.undoBtn.onclick=()=>{
+      this.paths.pop();
+      this.#redraw();
+    }
   }
   #redraw(){
     this.ctx.clearRect(0,0,
       this.canvas.width,this.canvas.height);
-    draw.path(this.ctx,this.path);
+    draw.paths(this.ctx,this.paths);
+    if(this.paths.length>0){
+      this.undoBtn.disabled=false;
+    }else{
+      this.undoBtn.disabled=true;
+    }
   }
   #getMouse=(evt)=>{
     const rect = this.canvas.getBoundingClientRect();
     return [
       Math.round(evt.clientX-rect.left),
-      Math.round(evt.clientY=rect.top)
+      Math.round(evt.clientY-rect.top)
     ];
   }
 }
